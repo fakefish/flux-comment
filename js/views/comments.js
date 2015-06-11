302f15a02c56a4a6356296fe44bc63e4a112301c
王佳裕 <fakefish.wjy@gmail.com>
2015-06-11 16:40:58 +0800

    from kit build first

--- js/views/comments.js
+++ js/views/comments.js
@@ -1,10 +1,41 @@
 var React = require('react');
+var CommentStore = require('../stores/comment-store');
+
+function getStateFromStore() {
+  return {
+    comments: CommentStore.getAll(),
+  };
+}
 
 var Comments = React.createClass({
 
+  onChange: function() {
+    this.setState(getStateFromStore());
+  },
+
+  getInitialState: function() {
+    return getStateFromStore();
+  },
+
+  commentDidMount: function() {
+    CommentStore.addChangeListener(this.onChange);
+  },
+
+  commentWillUnmount: function() {
+    CommentStore.removeChangeListener(this.onChange);
+  },
+
   render: function() {
     return (
-      <div />
+      <div className="comments">
+      {this.state.comments.map(function(comment,index) {
+        return (
+          <div className="comment" key={'comment-' + index}>
+            {comment.text}
+          </div>
+        );
+      })}
+      </div>
     );
   }
 
